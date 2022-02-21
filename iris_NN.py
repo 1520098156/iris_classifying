c27ea3628b8b0e12ea856e737efe143041555c66
1520098156@qq.com <1520098156@qq.com>
2022-02-21 18:16:59 +0800

    This is NN that classifies iris dataset

--- iris_NN.py
+++ iris_NN.py
@@ -6,6 +6,7 @@ def sigmoid(x):
     return 1 / (1 + np.exp(-x))
 
 
+# 损失函数
 def loss_function(a, y):
     return -(y * np.log(a) + (1 - y) * np.log(1 - a))
 
@@ -23,14 +24,6 @@ class iris_NN:
         self.X_train = X_train.T
         self.X_test = X_test.T
 
-        self.a2 = np.zeros((3, 38))
-
-        self.W1 = np.random.normal(0.0, 1, (8, 4))
-        self.W2 = np.random.normal(0.0, 1, (3, 8))
-        self.B1 = np.zeros((8, 1))
-        self.B2 = np.zeros((3, 1))
-
-    def train(self):
         oneHot = np.identity(3)
         for i in range(oneHot.shape[0]):
             for j in range(oneHot.shape[1]):
@@ -40,22 +33,29 @@ class iris_NN:
                     oneHot[i, j] = 0.01
 
         y_true = oneHot[self.y_train.T.astype(int)][0]
-        y_true = y_true.T
+        self.y_true = y_true.T
+
+        self.a2 = np.zeros((3, 38))
+
+        self.W1 = np.random.normal(0.0, 1, (8, 4))
+        self.W2 = np.random.normal(0.0, 1, (3, 8))
+        self.B1 = np.zeros((8, 1))
+        self.B2 = np.zeros((3, 1))
+
+    def train(self):
 
         W1 = np.random.normal(0.0, 1, (8, 4))
         W2 = np.random.normal(0.0, 1, (3, 8))
         B1 = np.zeros((8, 1))
         B2 = np.zeros((3, 1))
 
-
-
-        for i in range(5000):
+        for epoch in range(5000):
             out1 = np.dot(W1, self.X_train) + B1
             act1 = sigmoid(out1)
             out2 = np.dot(W2, act1) + B2
             act2 = sigmoid(out2)
 
-            dZ2 = act2 - y_true
+            dZ2 = act2 - self.y_true
             dW2 = 1 / 112 * np.dot(dZ2, act1.T)
             dB2 = 1 / 112 * np.sum(dW2, axis=1, keepdims=True)
 
@@ -68,21 +68,25 @@ class iris_NN:
             W1 -= self.lr * dW1
             B1 -= self.lr * dB1
 
+            # 每两百次输出一侧损失函数
+            # if epoch % 200 == 0:
+            #     print(np.sum(loss_function(act2, self.y_true)))
+
         self.W2 = W2
         self.B2 = B2
         self.W1 = W1
         self.B1 = B1
 
-        o1 = np.dot(W1, self.X_test) + B1
-        a1 = sigmoid(o1)
-        o2 = np.dot(W2, a1) + B2
-        self.a2 = sigmoid(o2)
-
     def test(self):
         result = []
 
-        for i in range(self.a2.T.shape[0]):
-            result.append(np.argmax(self.a2.T[i]))
+        o1 = np.dot(self.W1, self.X_test) + self.B1
+        a1 = sigmoid(o1)
+        o2 = np.dot(self.W2, a1) + self.B2
+        a2 = sigmoid(o2)
+
+        for i in range(a2.T.shape[0]):
+            result.append(np.argmax(a2.T[i]))
 
         true_no = 0
         for i in range(len(result)):
@@ -100,6 +104,8 @@ class iris_NN:
 
         for i in range(act2.T.shape[0]):
             result.append(np.argmax(act2.T[i]))
+
+        result = np.array([result])
 
         print('Prediction is:')
         print(result)
